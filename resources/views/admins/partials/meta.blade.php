d4bb56263e04b44657298a3a65a623707ed4e664
scotttresor <scotttresor@gmail.com>
2021-12-16 15:25:02 +0100

    [REV] review stats for application

--- resources/views/admins/partials/meta.blade.php
+++ resources/views/admins/partials/meta.blade.php
@@ -3,4 +3,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="description" content=" {{ config('app.name') }}">
-<link rel="shortcut icon" href="{{ asset('app/images/icon.png') }}">
+<link rel="shortcut icon" href="{{ asset('assets/admins/images/favicon ticket key.png') }}">
--- resources/views/companies/company.blade.php
+++ resources/views/companies/company.blade.php
@@ -5,5 +5,35 @@
 @endsection
 
 @section('content')
-
+    <div class="nk-content-inner">
+        <div class="nk-content-body">
+            <div class="nk-block-head nk-block-head-sm">
+                <div class="nk-block-between">
+                    <div class="nk-block-head-content">
+                        <h4 class="nk-block-title page-title">Dashboard</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="nk-block">
+                <div class="row g-gs">
+                    @include('companies.components._stat', [
+                        'username' => "Chauffeurs",
+                        'amount' => \App\Models\Driver::where('company_id', auth()->user()->company->id)->count()
+                    ])
+                    @include('companies.components._stat', [
+                        'username' => "Nombre bus",
+                        'amount' => \App\Models\Bus::where('company_id', auth()->user()->company->id)->count()
+                    ])
+                    @include('companies.components._stat', [
+                        'username' => "Ville",
+                        'amount' => \App\Models\Town::where('company_id', auth()->user()->company->id)->count()
+                    ])
+                    @include('companies.components._stat', [
+                        'username' => "Reservation",
+                        'amount' => \App\Models\Booking::where('company_id', auth()->user()->company->id)->count()
+                    ])
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
