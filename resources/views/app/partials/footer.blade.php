a94867252f895bbf15d2efa449fc0dd9474f8108
Dominique Youness <youness.dominique@gmail.com>
2022-07-26 13:27:12 +0200

    [style] remove unecessary information in footer

--- resources/views/app/partials/footer.blade.php
+++ resources/views/app/partials/footer.blade.php
@@ -3,74 +3,30 @@
         <div class="container">
             <div class="footer-links">
                 <div class="row">
-                    <div class="col-lg-3">
+                    <div class="col-lg-4">
                         <div class="footer-about footer-margin">
                             <div class="logo">
                                 <img src="{{ asset('assets/admins/images/logo.png') }}" height="50%" width="80%" alt="Image">
                             </div>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                            <div class="about-location">
-                                <ul>
-                                    <li><i class="flaticon-maps-and-flags" aria-hidden="true"></i> Location</li>
-                                    <li><i class="flaticon-phone-call"></i> (012)-345-6789</li>
-                                    <li><i class="flaticon-mail"></i> <a href="" class="__cf_email__">[email&#160;protected]</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-social-links">
-                                <ul>
-                                    <li class="social-icon"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                    <li class="social-icon"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                    <li class="social-icon"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                    <li class="social-icon"><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-                                    <li class="social-icon"><a href="#"><i class="fa fa-google" aria-hidden="true"></i></a></li>
-                                </ul>
-                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-lg-4">
+                        <div class="about-location">
+                            <ul>
+                                <li><i class="flaticon-maps-and-flags" aria-hidden="true"></i> Location</li>
+                                <li><i class="flaticon-phone-call"></i> (012)-345-6789</li>
+                                <li><i class="flaticon-mail"></i> <a href="" class="__cf_email__">info@bus.tiketkey.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
                         <div class="footer-links-list footer-margin">
-                            <h3>Browse Tour</h3>
                             <ul>
                                 <li><a href="#">Cyclades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
                                 <li><a href="#">North Ionian <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
                                 <li><a href="#">Sporades <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="footer-recent-post footer-margin">
-                            <h3>Recent Posts</h3>
-                            <ul>
-                                <li>
-                                    <div class="recent-post-item">
-                                        <div class="recent-post-image">
-                                            <img src="images/bucket1.jpg" alt="Image">
-                                        </div>
-                                        <div class="recent-post-content">
-                                            <h4><a href="#">A trip to heaven</a></h4>
-                                            <p>June 17, 2018</p>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="footer-links-list">
-                            <div class="footer-instagram">
-                                <h3>Instagram</h3>
-                                <ul>
-                                    <li><img src="images/insta1.jpg" alt="Image"></li>
-                                    <li><img src="images/insta2.jpg" alt="Image"></li>
-                                    <li><img src="images/insta3.jpg" alt="Image"></li>
-                                    <li><img src="images/insta4.jpg" alt="Image"></li>
-                                    <li><img src="images/insta5.jpg" alt="Image"></li>
-                                    <li><img src="images/insta6.jpg" alt="Image"></li>
-                                    <li><img src="images/insta7.jpg" alt="Image"></li>
-                                    <li><img src="images/insta8.jpg" alt="Image"></li>
-                                    <li><img src="images/insta9.jpg" alt="Image"></li>
-                                </ul>
-                            </div>
                         </div>
                     </div>
                 </div>
